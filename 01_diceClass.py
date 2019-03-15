9ccf629b8e7feffb4f22a1de71a2d51b9c538baf
Garrett Chen <g.chen4@share.epsb.ca>
2019-03-15 11:55:54 -0600

    cards and dice

--- 01_diceClass.py
+++ 01_diceClass.py
@@ -3,17 +3,20 @@ title: making dice
 author: garrett
 date created: 2019-03-14
 '''
-import random
+
 class die:
-    def __init__(self):
-        self.sides = 6
-        self.values = [1, 2, 3, 4, 5, 6]
+    import random
+    def __init__(self, sides):
+        self.sides = sides
+        self.values = []
+        self.setNumSides(sides)
 
     def getRollVal(self):
-        return self.values[random.randrange(len(self.values))]
+        return self.values[die.random.randrange(len(self.values))]
 
     def setNumSides(self, sides):
         self.sides = sides
+        self.setSideVal(self.sides)
 
     def getNumSides(self):
         return self.sides
@@ -24,16 +27,35 @@ class die:
             templi.append(int(input("value")))
         self.values = templi
 
-die1 = die() # constructing object from class
+class player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.dice = [die(3), die(3), die(3), die(3)]
+
+    def getScore(self):
+        return self.score
+
+    def calcScore(self):
+        for i in range(len(self.dice)):
+            self.score += self.dice[i].getRollVal()
+
+garrett = player('garrett')
+garrett.calcScore()
+print(garrett.getScore())
+#die1 = die(5) # constructing object from class
+#print(die1.getRollVal())
+'''
 die2 = die()
 
 print(die1.getNumSides(), die2.getNumSides())
 
-die1.setNumSides(8)
+# die1.setNumSides(8)
 die2.setNumSides(4)
 
 print(die1.getNumSides(), die2.getNumSides())
 
-die2.setSideVal(die2.getNumSides())
+# die2.setSideVal(die2.getNumSides())
 
 print(die2.getRollVal())
+'''
---
+++ cards.py
@@ -0,0 +1,13 @@
+
+'''
+title: 
+author: garrett
+date created: 2019-03-15
+'''
+
+class card:
+    def __init__(self):
+        self.size = size
+        self.suit = suit
+        self.number = 13
+class deck:
